6abe1bccf149515872a967f0551868cac196b260
Sohesh TS <soheshts@gmail.com>
2025-09-19 19:11:34 +0530

    Initial commit

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('radio-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/app.js',
+        '/streams.json',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
